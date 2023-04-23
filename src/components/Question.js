4e8ac9c36fedd9b27e40e3741e4f546b072170ac
Noah <noahmwangi@gmail.com>
2023-04-23 17:53:44 +0300

    Completed Assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  // add useEffect code - create a side effect to run a callback function after 1 second
+  useEffect(() => {
+    //decrease the amount of time remaining by 1 every 1 second
+    const timer = setTimeout(() => {
+      setTimeRemaining(last => last-1)
+    }, 1000)
+
+//third deliverable
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered = false;
+    }
+// cleanup function to clean up the setTimeout async function that is running in the background
+    return function cleanup() {
+      clearTimeout(timer);
+    };
+
+  }, [])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
